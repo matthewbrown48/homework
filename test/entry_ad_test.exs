d4f10e710fa8c241ff626e2de778e43b19cdde08
Matthew Brown <matthewbrown48@outlook.com>
2021-02-23 20:24:33 -0700

    entry_ad_test

---
+++ test/entry_ad_test.exs
@@ -0,0 +1,48 @@
+defmodule LargeDomTest do
+  # Import helpers
+  use Hound.Helpers
+  use ExUnit.Case
+
+  # Start hound session and destroy when tests are run
+  hound_session()
+
+    def open_page(url, pgTitle) do
+      navigate_to url
+      wait_for_page_load()
+      #Return True if page title is correct
+      page_title() == pgTitle
+  end
+  
+  def wait_for_page_load() do
+    #Quick pause to allow the page to load, before checking if it's loaded
+    :timer.sleep(750)
+    case execute_script("return document.readyState") do
+       "loading"  ->  wait_for_page_load()
+       _ -> true 
+     end
+  end
+
+
+  test "ClearEntryAd" do
+    #navigate_to "https://the-internet.herokuapp.com/login", and confirm that the page loaded correctly
+    assert open_page("https://the-internet.herokuapp.com/entry_ad", "The Internet") == true
+    #Get the elements we'll be using in this test case
+    div_Modal = find_element(:id, "modal")
+    #Confirm the ad popped up
+    assert visible_in_page?(~r/THIS IS A MODAL WINDOW/) == true
+    #Close the ad
+    div_Close = find_element(:xpath, "//p[text()='Close']")
+    click(div_Close)
+    :timer.sleep(500)
+    #Confirm the popup is gone
+    assert visible_in_page?(~r/THIS IS A MODAL WINDOW/) == false
+    #In the requirements, we are told "If closed, it will not appear on subsequent page loads."
+    #To test this, we'll refresh the page a few times to ensure that the popup does not return
+    #You can change 2 in the line below to whatever number of times you want it to be, minus 1
+    Enum.each(0..2, fn(x) ->
+      refresh_page()
+      wait_for_page_load()
+      assert visible_in_page?(~r/THIS IS A MODAL WINDOW/) == false
+    end)
+  end
+end
--- test/formvalidation_test.exs
+++ test/formvalidation_test.exs
@@ -5,15 +5,76 @@ defmodule FormValidationTest do
 
   # Start hound session and destroy when tests are run
   hound_session()
+  #Test credentials can be changed here
+  @username :tomsmith
+  @password :"SuperSecretPassword!"
 
-  test "Open form " do
-    navigate_to "https://the-internet.herokuapp.com/login"
+  def open_page(url, pgTitle) do
+      navigate_to url
+      wait_for_page_load()
+      #Return True if page title is correct
+      page_title() == pgTitle
+  end
+
+  def wait_for_page_load() do
+    #Quick pause of 500 ms to allow the page to load, before checking if it's loaded
+    :timer.sleep(500)
+    case execute_script("return document.readyState") do
+       "loading"  ->  wait_for_page_load()
+       _ -> true 
+     end
+  end
+
+
+  test "SubmitWithoutUserName" do
+    #navigate_to "https://the-internet.herokuapp.com/login", and confirm that the page loaded correctly
+    assert open_page("https://the-internet.herokuapp.com/login", "The Internet") == true
+    #Get the elements we'll be using in this test case
+    fld_Username = find_element(:id, "username")
+    fld_Password = find_element(:id, "password")
+    frm_Form = find_element(:id, "login")
+    #Fill Password, and submit without username
+    fill_field(fld_Password, @password)
+    submit_element(frm_Form)   
+    assert {:ok, el_alert} = search_element(:id, "flash")
+    str_alertText = visible_text(el_alert)
+    #Confirm Correct error
+    assert str_alertText =~ "Your username is invalid!"
+  end
     
+  test "SubmitWithoutPassword" do
+    #navigate_to "https://the-internet.herokuapp.com/login", and confirm that the page loaded correctly
+    assert open_page("https://the-internet.herokuapp.com/login", "The Internet") == true
+    #Get the elements we'll be using in this test case
+    fld_Username = find_element(:id, "username")
+    fld_Password = find_element(:id, "password")
+    frm_Form = find_element(:id, "login")
+    #Fill username, and submit without password
+    fill_field(fld_Username, @username)
+    fill_field(fld_Password, "")#Clearing the field
+    submit_element(frm_Form)   
+    assert {:ok, el_alert} = search_element(:id, "flash")
+    str_alertText = visible_text(el_alert)
+    #Confirm Correct error
+    assert str_alertText =~ "Your password is invalid!"
   end
 
-  test "Open Form" do
-    navigate_to "https://the-internet.herokuapp.com/login"
-    assert page_title() == "The Internet" 
+  test "Login" do
+    #navigate_to "https://the-internet.herokuapp.com/login", and confirm that the page loaded correctly
+    assert open_page("https://the-internet.herokuapp.com/login", "The Internet") == true
+    #Get the elements we'll be using in this test case
+    fld_Username = find_element(:id, "username")
+    fld_Password = find_element(:id, "password")
+    frm_Form = find_element(:id, "login")
+    #Fill username, and submit without password
+    fill_field(fld_Username, @username)
+    fill_field(fld_Password, @password)#Clearing the field
+    submit_element(frm_Form)   
+    assert {:ok, el_alert} = search_element(:id, "flash")
+    str_alertText = visible_text(el_alert)
+    #Confirm Correct error
+    assert str_alertText =~ "You logged into a secure area!"
   end
+
 
 end
